7722c9f6f4bca2d4a9a04db1215077755d7df7ec
Mehedi Hasan Shanto <shanto.360@yahoo.com>
2022-03-14 23:25:40 +0600

    country-name

---
+++ Rest-Country/js/contries.js
@@ -0,0 +1,20 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data))
+}
+const displayCountries = countries =>{
+    console.log(countries)
+    const CountriesHTML = countries.map(country => getCountryHTML(country))
+    // console.log(CountriesHTML[0])
+    const container = document.getElementById('countries');
+    container.innerHTML = CountriesHTML
+}
+const getCountryHTML = country => {
+    return`
+    <div>
+        <h2>${country.name.common}</h2>
+    </div>
+    `
+}
+loadCountries()
