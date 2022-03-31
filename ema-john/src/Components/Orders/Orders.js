cff2c2c57471204bf8e6e14e09bb39c7dca1a9d3
shanto360 <shanto.360@yahoo.com>
2022-03-31 19:10:57 +0600

    order-usestate

--- ema-john/src/Components/Orders/Orders.js
+++ ema-john/src/Components/Orders/Orders.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 
 const Orders = () => {
+    const [products] = useProducts();
     return (
         <div>
-            <h2>this is order page</h2>
+            <h2>this is order :{products.length}</h2>
         </div>
     );
 };
